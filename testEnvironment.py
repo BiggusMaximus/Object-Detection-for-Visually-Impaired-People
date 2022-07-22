1111f0855edfe0abd3890afa15f55375f1883161
BiggusMaximus <annastyabagas164@gmail.com>
2022-07-22 12:58:00 +0700

    adding new env check

---
+++ testEnvironment.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+import platform
+import cv2
+
+print(f"Python version : {platform.python_version()}")
+print(f"tensorflow version : {tf.__version__}")
+print(f"tensorflow_datasets version : {cv2.__version__}")
+print(f"numpy version : {np.version.version}")
